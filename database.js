6b419d36cee94cc233bb2f237e734a52d0fa429b
enzzzoooo <lorenzo_ambrosio@dlsu.edu.ph>
2024-04-09 10:23:02 +0800

    removed extra routes

--- database.js
+++ database.js
@@ -23,20 +23,7 @@ export async function databaseGetAppointments(){
     return rows
 }
 
-export async function databaseGetClinics(){
-    const [rows]= await pool.query("SELECT * FROM clinics")
-    return rows
-}
 
-export async function databaseGetDoctors(){
-    const [rows]= await pool.query("SELECT * FROM doctors")
-    return rows
-}
-
-export async function databaseGetPx(){
-    const [rows]= await pool.query("SELECT * FROM px")
-    return rows
-}
 
 // CREATE FUNCTIONS
 
@@ -51,38 +38,6 @@ export async function createAppointment(pxid, clinicid, doctorid, status, TimeQu
     }
 }
 
-export async function createClinic(clinicid, hospitalname, IsHospital, City, Province, RegionName) {
-    try {
-        const result = await pool.query(`
-            INSERT INTO clinics (clinicid, hospitalname, IsHospital, City, Province, RegionName)
-            VALUES (?, ?, ?, ?, ?, ?)`, [clinicid, hospitalname, IsHospital, City, Province, RegionName]);
-    } catch (error) {
-        console.error("Error in createClinic function:", error);
-        throw error;
-    }
-}
-
-export async function createDoctor(doctorid, mainspecialty, age) {
-    try {
-        const result = await pool.query(`
-            INSERT INTO doctors (doctorid, mainspecialty, age)
-            VALUES (?, ?, ?)`, [doctorid, mainspecialty, age]);
-    } catch (error) {
-        console.error("Error in createDoctor function:", error);
-        throw error;
-    }
-}
-
-export async function createPx(pxid, age, gender) {
-    try {
-        const result = await pool.query(`
-            INSERT INTO px (pxid, age, gender)
-            VALUES (?, ?, ?)`, [pxid, age, gender]);
-    } catch (error) {
-        console.error("Error in createPx function:", error);
-        throw error;
-    }
-}
 
 
 
@@ -132,56 +87,6 @@ export async function updateAppointment(appid, pxid, clinicid, doctorid, status,
     }
 }
 
-export async function updateClinic(clinicid, hospitalname, IsHospital, City, Province, RegionName) {
-    try {
-        const result = await pool.query(`
-            UPDATE clinics 
-            SET hospitalname = ?, IsHospital = ?, City = ?, Province = ?, RegionName = ?
-            WHERE clinicid = ?`, [hospitalname, IsHospital, City, Province, RegionName, clinicid]);
-
-        if (result.affectedRows === 0) {
-            throw new Error(`Record with clinicid ${clinicid} not found.`);
-        }
-        return result;
-    } catch (error) {
-        console.error("Error in updateClinic function:", error);
-        throw error;
-    }
-}
-
-export async function updateDoctor(doctorid, mainspecialty, age) {
-    try {
-        const result = await pool.query(`
-            UPDATE doctors 
-            SET mainspecialty = ?, age = ?
-            WHERE doctorid = ?`, [mainspecialty, age, doctorid]);
-
-        if (result.affectedRows === 0) {
-            throw new Error(`Record with doctorid ${doctorid} not found.`);
-        }
-        return result;
-    } catch (error) {
-        console.error("Error in updateDoctor function:", error);
-        throw error;
-    }
-}
-
-export async function updatePx(pxid, age, gender) {
-    try {
-        const result = await pool.query(`
-            UPDATE px 
-            SET age = ?, gender = ?
-            WHERE pxid = ?`, [age, gender, pxid]);
-
-        if (result.affectedRows === 0) {
-            throw new Error(`Record with pxid ${pxid} not found.`);
-        }
-        return result;
-    } catch (error) {
-        console.error("Error in updatePx function:", error);
-        throw error;
-    }
-}
 
 export async function updateDataLuzon(id, city, province) {
     try {
